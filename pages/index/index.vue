41b60db9a57c2182d5c75dcf374e234e535e5524
DYZ1995 <rationaldyz@163.com>
2019-09-11 22:52:57 +0800

    第一个微信小程序

---
+++ pages/index/index.vue
@@ -0,0 +1,67 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/photo.png"></image>
+		<view class="text-area">
+			<text class="title" @click="goToInfoList()">{{title}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: '点我查看新闻哦~'
+			}
+		},
+		onLaunch: function() {
+		    console.log('App Launch')
+		},
+		onShow: function() {
+		    console.log('App Show')
+		},
+		onHide: function() {
+		    console.log('App Hide')
+		},
+		onLoad() {
+			console.log(this.$mp.page.route);
+			
+		},
+		methods: {
+			goToInfoList() {
+				uni.navigateTo({
+					url: '/pages/info/list?content=我是参数'
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200upx;
+		width: 200upx;
+		margin-top: 200upx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50upx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
+	}
+</style>
